196ac59d808361ffb824b7b0da2e34b6287f312c
raoul2000 <manu34@free.fr>
2019-03-07 18:04:12 +0100

    add link to go from order view to related  transaction view

--- src/views/order/view.php
+++ src/views/order/view.php
@@ -17,6 +17,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <p>
         <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Go To Transaction', ['transaction/view', 'id' => $model->transaction_id], ['class' => 'btn btn-default']) ?>
         <?= Html::a('Delete', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
