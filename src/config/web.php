d16e5baf42328c80bffe3fd6bf325c6d32484467
raoul2000 <manu34@free.fr>
2019-04-05 16:48:02 +0200

    set source language

--- src/config/web.php
+++ src/config/web.php
@@ -7,6 +7,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'language' => 'fr-FR',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
