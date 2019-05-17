dbb3f7e5890c7e1419de01e9e4de89eeb7070b9b
raoul2000 <manu34@free.fr>
2019-05-17 13:00:06 +0200

    add warning if no contact exist

--- src/views/common/user-contact.php
+++ src/views/common/user-contact.php
@@ -12,21 +12,29 @@ use yii\widgets\ActiveForm;
 <div class="user-contact-form">
     <h1>User Contact</h1>
     <hr/>
-    <?php $form = ActiveForm::begin(); ?>
+    <?php if (count($contactNames) === 0): ?>
+        <div class="alert alert-warning" role="alert">
+            <span class="glyphicon glyphicon-warning-sign" aria-hidden="true"></span>
+            <b>No Contact available</b><br/>
+            Please <?= Html::a('click here', ['contact/create'],['class'=> 'alert-link']) ?> to create a contact.
+        </div>        
+    <?php else: ?>
+        <?php $form = ActiveForm::begin(); ?>
 
-        <?= \dosamigos\selectize\SelectizeDropDownList::widget([
-            'name' => Html::getInputName($model, 'contact_id'),
-            'value' => $model->contact_id,
-            'items' => $contactNames,
-            'clientOptions' => [
-                // ...
-            ],
-        ]); ?>
+            <?= \dosamigos\selectize\SelectizeDropDownList::widget([
+                'name' => Html::getInputName($model, 'contact_id'),
+                'value' => $model->contact_id,
+                'items' => $contactNames,
+                'clientOptions' => [
+                    // ...
+                ],
+            ]); ?>
 
-        <div class="form-group">
-            <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
-            <?= Html::a('Cancel', $redirect_url, ['class' => 'btn btn-default']) ?>
-        </div>
+            <div class="form-group">
+                <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+                <?= Html::a('Cancel', $redirect_url, ['class' => 'btn btn-default']) ?>
+            </div>
 
-    <?php ActiveForm::end(); ?>
+        <?php ActiveForm::end(); ?>
+    <?php endif;?>
 </div>
