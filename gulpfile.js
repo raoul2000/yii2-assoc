81f920716d2edc68e96d870a45573c03b5a47e84
raoul2000 <manu34@free.fr>
2019-05-25 18:45:13 +0200

    more gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const { task1 } = require('./gulp-task/task1');
 const del = require('del');
 const notify = require('gulp-notify');
 const rename = require("gulp-rename");
+const exec = require('child_process').exec;
 
 function clean() {
     return del('./build/**', { force: true });
@@ -11,13 +12,21 @@ function clean() {
 
 function copy() {
     return src([
-        './src/assets/**',
-        './src/commands/**',
-        './src/components/**',
+        './src/**',
+        '!./src/runtime/*/**',
+        '!./src/vendor/**',
+        '!./src/web/assets/*/**'
     ], { base: './src/' })
         .pipe(dest('./build'));
 }
 
+function createFolders() {
+    return src('*.*', {read : false})
+    .pipe( dest('./build/runtime'))
+    .pipe( dest('./build/runtime/cache'))
+    .pipe( dest('./build/runtime/logs'));
+
+}
 function copyConfig() {
     return src(
         [
@@ -35,6 +44,22 @@ function copyConfig() {
         .pipe(dest('./build'));
 }
 
+function ping() {
+    return new Promise( (resolve, reject) => {
+        exec('ping localhost', function (err, stdout, stderr) {
+            if(err) {
+                reject(err);
+            } else {
+                console.log(stdout);
+                console.log(stderr);
+                resolve(true);
+            }
+          });      
+    });
+}
+
 exports.copy = copy;
 exports.clean = clean;
-exports.default = series(clean, copyConfig);
+exports.ping = ping;
+//exports.default = series(clean, copyConfig, createFolders);
+exports.default = series(clean, copy);
