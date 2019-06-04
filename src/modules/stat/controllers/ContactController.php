29161fe5cf7e1f504fb783b591f37326b5061f0c
raoul2000 <manu34@free.fr>
2019-06-04 09:57:50 +0200

    fix : stat query groupby

--- src/modules/stat/controllers/ContactController.php
+++ src/modules/stat/controllers/ContactController.php
@@ -21,40 +21,40 @@ class ContactController extends \yii\web\Controller
         ])->count();
 
         $results = Yii::$app->db->createCommand(
-            'SELECT count(id) as total, gender, YEAR(CURDATE()) - YEAR(birthday) AS age  
+            'SELECT count(*) as total, gender, YEAR(CURDATE()) - YEAR(birthday) AS age  
              FROM contact 
              where 
                 is_natural_person is TRUE 
                 and (gender = 2 or gender = 1) 
-                and birthday is not null 
-            group by age 
-            order by age;')
-            ->queryAll();
+                and birthday is not null
+            group by age, gender;'
+        )->queryAll();
+        
         $serieMan = array_fill(0, 200, 0);
         $serieWom = array_fill(0, 200, 0);
         $serie = null;
         $maxAge = 0;
-        foreach($results as $result) {
+        foreach ($results as $result) {
             $age = intVal($result['age']);
             $total = intval($result['total']);
 
             if ($result['gender'] == 1) {
-                $serieMan[$age] = $total;    
+                $serieMan[$age] = $total;
             } else {
-                $serieWom[$age] = $total;    
+                $serieWom[$age] = $total;
             }
 
-            if( $result['age'] > $maxAge) {
+            if ($result['age'] > $maxAge) {
                 $maxAge = $age;
             }
         }
-        $serieMan = array_slice($serieMan,0, $maxAge);
-        $serieWom = array_slice($serieWom,0, $maxAge);
+        $serieMan = array_slice($serieMan, 0, $maxAge);
+        $serieWom = array_slice($serieWom, 0, $maxAge);
 
         return $this->render('index', [
             'countPerson' => $countPerson,
             'serieMan' => $serieMan,
             'serieWom' => $serieWom
-        ]);        
+        ]);
     }
 }
