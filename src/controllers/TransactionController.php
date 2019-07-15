8cc25a9169df9d70fcee6a481bb7720eedb288e8
raoul2000 <manu34@free.fr>
2019-07-15 11:29:17 +0200

    improve transaction view-complete render

--- src/controllers/TransactionController.php
+++ src/controllers/TransactionController.php
@@ -351,9 +351,9 @@ class TransactionController extends Controller
          * Insert transactions ofr this order into the transaction array
          * if not already present
          */
-        $addTransactions = function($order, $transactions) {
+        $addTransactions = function ($order, $transactions) {
             $transactionAdded = false;
-            foreach($order->transactions as $transaction) {
+            foreach ($order->transactions as $transaction) {
                 $key = 'id:'+$transaction->id;
                 if ( ! array_key_exists($key, $transactions)) {
                     $transactionAdded = true;
@@ -365,9 +365,9 @@ class TransactionController extends Controller
         /**
          * insert orders for this transaction into an array (avoiding duplicate)
          */
-        $addOrders = function($transaction, $orders) {
+        $addOrders = function ($transaction, $orders) {
             $orderAdded = false;
-            foreach($transaction->orders as $order) {
+            foreach ($transaction->orders as $order) {
                 $key = 'id:'+$order->id;
                 if ( ! array_key_exists($key, $orders)) {
                     $orderAdded = true;
@@ -385,27 +385,37 @@ class TransactionController extends Controller
         $transactions['id:'+$transactionModel->id] = $transactionModel;
 
         $transactionAdded = true;
-        while($transactionAdded) {
-            foreach($transactions as $transaction) {
+        while ($transactionAdded) {
+            foreach ($transactions as $transaction) {
                 list($added, $orders) = $addOrders($transaction, $orders);
             }
             $transactionAdded = false;
-            foreach($orders as $order) {
+            foreach ($orders as $order) {
                 list($added, $transactions) = $addTransactions($order, $transactions);
-                if( $added ) {
+                if ($added) {
                     $transactionAdded = true;
                 }
             }
         }
 
+        // comput transaction and order values sum
+        $valueReducer = function ($acc, $current) {
+            return $acc + ( is_numeric($current->value) ? $current->value : 0);
+        };
+
+        $transactionValueSum = array_reduce($transactions, $valueReducer);
+        $orderValueSum = array_reduce($orders, $valueReducer);
+
         return $this->render('view-complete', [
             'transactions' => $transactions,
             'orders' => $orders,
+            'transactionValueSum' => array_reduce($transactions, $valueReducer),
+            'orderValueSum' => array_reduce($orders, $valueReducer),
             'bankAccounts' => \app\models\BankAccount::getNameIndex(),
             'products' => \app\models\Product::getNameIndex(),
             'contacts' => \app\models\Contact::getNameIndex(),
             'transactionType' => \app\components\Constant::getTransactionTypes(),
-        ]);        
+        ]);
     }
 
     /**
--- src/views/transaction/view-complete.php
+++ src/views/transaction/view-complete.php
@@ -11,10 +11,23 @@ use yii\helpers\ArrayHelper;
 $this->title = 'complete';
 $this->params['breadcrumbs'][] = ['label' => 'Transactions', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
-\yii\web\YiiAsset::register($this);
-$transactionValueSum = $orderValueSum = 0;
+
+$valueSumRowClass = '';
+$withOrdersAndTransactions = false;
+if (count($orders) && count($transactions)) {
+    $withOrdersAndTransactions = true;
+    $valueSumRowClass = ( $transactionValueSum == $orderValueSum ? 'bg-success' : 'bg-warning');
+}
 ?>
 <div class="transaction-view">
+
+    <?php if ($withOrdersAndTransactions && $transactionValueSum != $orderValueSum): ?>
+        <div class="alert alert-warning" role="alert">
+            <b>Attention</b> : La somme de toutes les transactions (<?= $transactionValueSum ?>) 
+            n'est pas égale à la somme des commandes (<?= $orderValueSum ?>)
+        </div>
+    <?php endif; ?>
+
     <h2>
         <span class="glyphicon glyphicon-transfer" aria-hidden="true"></span>  
         Transactions
@@ -34,11 +47,23 @@ $transactionValueSum = $orderValueSum = 0;
             </tr>
         </thead>        
         <tbody>        
-            <?php foreach($transactions as $transaction): ?>
+            <?php foreach ($transactions as $transaction): ?>
                 <tr>
                     <td><?= $transaction->id ?></td>
-                    <td><?= $bankAccounts[$transaction->from_account_id] ?></td>
-                    <td><?= $bankAccounts[$transaction->to_account_id] ?></td>
+                    <td>
+                        <?= Html::a(
+                            Html::encode($bankAccounts[$transaction->from_account_id]),
+                            ['bank-account/view', 'id' => $transaction->from_account_id ],
+                            ['title' => 'view Account']
+                        )?>                                         
+                    </td>
+                    <td>
+                        <?= Html::a(
+                            Html::encode($bankAccounts[$transaction->to_account_id]),
+                            ['bank-account/view', 'id' => $transaction->to_account_id ],
+                            ['title' => 'view Account']
+                        )?>                                         
+                    </td>
                     <td><?= $transaction->reference_date ?></td>
                     <td><?= $transactionType[$transaction->type] ?></td>
                     <td><?= $transaction->code ?></td>
@@ -47,35 +72,33 @@ $transactionValueSum = $orderValueSum = 0;
                         <?= Html::a(
                             '<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>',
                             ['transaction/view', 'id' => $transaction->id],
-                            ['title' => 'view']
+                            ['title' => 'view Transaction']
                         ) ?>                        
                     </td>
-                    <?php
-                        $transactionValueSum += $transaction->value;
-                    ?>
                 </tr>
             <?php endforeach; ?>    
-            <tr style="font-weight:bold; font-size:1.2em;background-color:#eee">
+            <tr class="<?= $valueSumRowClass ?>">
                 <td colspan="6" style="text-align:right">
                     Montant Total des transactions :
                 </td>
-                <td>
-                        <?= $transactionValueSum ?>
+                <td style="border-top-width: 3px;border-top-color: #b1b1b1;">
+                    <b><?= $transactionValueSum ?></b>
                 </td>
                 <td></td>
             </tr>
         </tbody>
     </table>    
 
-    <h2>
-        Orders
-    </h2>
-    <hr/>
-    <?php if(count($orders) == 0): ?>
+
+    <?php if (count($orders) == 0): ?>
         <div class="alert alert-info" role="alert">
             There is no order related to transactions
         </div>
     <?php else: ?>
+        <h2>
+            Orders
+        </h2>
+        <hr/>    
         <table class="table table-condensed table-hover">
             <thead>
                 <tr>
@@ -87,30 +110,45 @@ $transactionValueSum = $orderValueSum = 0;
                 </tr>
             </thead>        
             <tbody>        
-                <?php foreach($orders as $order): ?>
+                <?php foreach ($orders as $order): ?>
                     <tr>
-                        <td><?= $contacts[$order->from_contact_id]?></td>
-                        <td><?= $contacts[$order->to_contact_id]?></td>
-                        <td><?= $products[$order->product_id]?></td>
+                        <td>
+                            <?= Html::a(
+                                Html::encode($contacts[$order->from_contact_id]),
+                                ['contact/view', 'id' => $order->from_contact_id ],
+                                ['title' => 'view Contact']
+                            )?>
+                        </td>
+                        <td>
+                            <?= Html::a(
+                                Html::encode($contacts[$order->to_contact_id]),
+                                ['contact/view', 'id' => $order->to_contact_id ],
+                                ['title' => 'view Contact']
+                            )?>
+                        </td>
+                        <td>
+                            <?= Html::a(
+                                Html::encode($products[$order->product_id]),
+                                ['product/view', 'id' => $order->product_id ],
+                                ['title' => 'view Product']
+                            )?>                        
+                        </td>
                         <td><?= $order->value?></td>
                         <td>
                             <?= Html::a(
                                 '<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>',
                                 ['order/view', 'id' => $order->id],
-                                ['title' => 'view']
+                                ['title' => 'view Order']
                             ) ?>                        
                         </td>
-                        <?php
-                            $orderValueSum += $order->value;
-                        ?>
                     </tr>
                 <?php endforeach; ?>    
-                <tr style="font-weight:bold; font-size:1.2em;background-color:#eee">
+                <tr class="<?= $valueSumRowClass ?>">
                     <td colspan="3" style="text-align:right">
                         Montant Total Des Produits : 
                     </td>
-                    <td>
-                        <?= $orderValueSum ?>
+                    <td style="border-top-width: 3px;border-top-color: #b1b1b1;">
+                        <b><?= $orderValueSum ?></b>
                     </td>
                     <td></td>
                 </tr>
