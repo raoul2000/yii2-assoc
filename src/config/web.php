34aed77c15e290b6e70bd0e2ac1d915c6c366ae2
raoul2000 <manu34@free.fr>
2019-08-22 18:23:15 +0200

    change app name

--- src/config/web.php
+++ src/config/web.php
@@ -6,6 +6,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'name' => 'Bob',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'language' => 'fr-FR',
