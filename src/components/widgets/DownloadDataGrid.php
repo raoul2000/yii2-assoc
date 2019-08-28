0ad1271030c736893955c804e6fcb447c89f3c0e
raoul2000 <manu34@free.fr>
2019-08-28 13:33:50 +0200

    add download report JS method (test)

--- src/components/widgets/DownloadDataGrid.php
+++ src/components/widgets/DownloadDataGrid.php
@@ -36,8 +36,64 @@ class DownloadDataGrid extends Widget
     */
     public function run()
     {
-        $this->registerJs();
+        //$this->registerJs();
+        $this->registerJs2();
         return $this->createButton();
+    }
+
+    public function registerJs2()
+    {
+        $headerName = self::TRIGGER_HEADER_NAME;
+        $js=<<<EOS
+        // This will hold the the file as a local object URL
+        var _OBJECT_URL;
+        
+        // Call an AJAX
+        document.getElementById('btn-export-report').addEventListener('click', function() {
+            var request = new XMLHttpRequest();
+            
+            request.addEventListener('readystatechange', function(e) {
+                if(request.readyState == 2 && request.status == 200) {
+                    // Download is being started
+                }
+                else if(request.readyState == 3) {
+                    // Download is under progress
+                }
+                else if(request.readyState == 4) {
+                    // Downloaing has finished
+        
+                    _OBJECT_URL = URL.createObjectURL(request.response);
+        
+                    // Set href as a local object URL
+                    document.querySelector('#save-file').setAttribute('href', _OBJECT_URL);
+                    
+                    // Set name of download
+                    document.querySelector('#save-file').setAttribute('download', 'img.jpeg');
+                    
+                    // Recommended : Revoke the object URL after some time to free up resources
+                    // There is no way to find out whether user finished downloading
+                    setTimeout(function() {
+                        window.URL.revokeObjectURL(_OBJECT_URL);
+                    }, 60*1000);
+                }
+            });
+            
+            request.addEventListener('progress', function(e) {
+                var percent_complete = (e.loaded / e.total)*100;
+                console.log(percent_complete);
+            });
+            
+            request.responseType = 'blob';
+            
+            // Downloading a JPEG file
+            request.open('get', document.location.href); 
+            request.setRequestHeader('$headerName', true);
+            
+            request.send(); 
+        });        
+EOS;
+
+        $this->getView()->registerJs($js, View::POS_READY, 'download-data-grid');
     }
 
     public function registerJs()
