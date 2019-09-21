d7c9241114c527bfbe124131df7cf9fa80ff97ec
raoul2000 <manu34@free.fr>
2019-09-21 12:34:27 +0200

    prevent multiselection of the same product

--- src/modules/gymv/views/registration/_product-select.js
+++ src/modules/gymv/views/registration/_product-select.js
@@ -5,17 +5,23 @@
     
     const addToSelectedProducts = (product) => {
         console.log("adding product ", product);
-        const html = `
-        <div class="selected-product-item" data-item-id="${product.id}">
-            <div class="product-remove" title="remove">
-                <span data-action="remove" class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-            </div>
-            <span class="product-name">
-                <span class="glyphicon glyphicon-gift" aria-hidden="true"></span> ${product.name}
-            </span>
-            <input type="hidden" name="ProductSelectionForm[product_ids][]" value="${product.id}"/>
-        </div>`;
-        el.selectedProductList.insertAdjacentHTML('afterbegin',html);
+        // prevent multiselection of the same product
+        const productAlreadySelected = el.selectedProductList.querySelector('div.selected-product-item[data-item-id="' + product.id + '"]');
+        if(productAlreadySelected) {
+            alert(product.name + "\nproduct already selected");
+        } else {
+            const html = `
+            <div class="selected-product-item" data-item-id="${product.id}">
+                <div class="product-remove" title="remove">
+                    <span data-action="remove" class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                </div>
+                <span class="product-name">
+                    <span class="glyphicon glyphicon-gift" aria-hidden="true"></span> ${product.name}
+                </span>
+                <input type="hidden" name="ProductSelectionForm[product_ids][]" value="${product.id}"/>
+            </div>`;
+            el.selectedProductList.insertAdjacentHTML('afterbegin',html);
+        }
     }
 
     const handleProductAction = (ev) => {
