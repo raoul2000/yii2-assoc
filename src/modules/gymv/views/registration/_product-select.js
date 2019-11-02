385227625407acc9ecc0a7403b516da3f9c88413
raoul2000 <manu34@free.fr>
2019-11-02 18:20:43 +0100

    fix: show/hide assurance checkbox

--- src/modules/gymv/views/registration/_product-select.js
+++ src/modules/gymv/views/registration/_product-select.js
@@ -1,16 +1,20 @@
 (function () {
     const el = {
-        "selectedProductList" : document.getElementById('selected-product-list'),
-
-        "achat_licence" : document.getElementById('achat_licence'),
-        "container_assurance" : document.getElementById('container-assurance'),
+        "selectedProductList"     : document.getElementById('selected-product-list'),
+        "achat_licence"           : document.getElementById('achat_licence'),
+        "container_assurance"     : document.getElementById('container-assurance'),
         "container_achat_licence" : document.getElementById('container-achat-licence')
     };
     
     const escapeHTML = (txt) => document.createElement('div').appendChild(document.createTextNode(txt)).parentNode.innerHTML; 
 
+    /**
+     * Add a Product to the currently selected product list.
+     * Creates an renders a new HTML element representing the newly added product
+     * If the same product is already selected, displays an alert error message
+     * @param {object} product product to add
+     */
     const addToSelectedProducts = (product) => {
-        console.log("adding product ", product);
         // prevent multiselection of the same product
         const productAlreadySelected = el.selectedProductList.querySelector('div.selected-product-item[data-item-id="' + product.id + '"]');
         if(productAlreadySelected) {
@@ -37,6 +41,10 @@
         }
     }
 
+    /**
+     * Remove an item from the product selection list
+     * @param {Event} ev the DOM event
+     */
     const handleProductAction = (ev) => {
         const action = ev.target.dataset.action;
         switch(action) {
@@ -48,28 +56,26 @@
                 break;
         }
     };
-    const handlerAssuranceChoice = (ev) => {
-        console.log('handlerAssuranceChoice ' + ev.target.selectedOptions[0].value);
-
-        el.container_assurance.style.visibility = ev.target.selectedOptions[0].dataset.hasOwnProperty('showOnSelect')
+    /**
+     * show/hide the "assurance" checkbox depending on currently selected option 
+     * element and the fact it owns a showOnSelect data attribute
+     */
+    const handlerAssuranceChoice = () => {
+        el.container_assurance.style.visibility = el.achat_licence.selectedOptions[0].dataset.hasOwnProperty('showOnSelect')
             ? 'visible'
             : 'hidden';
-/*            
-
-        const selectedOption = ev.target.selectedOptions[0];
-        if( selectedOption.dataset.hasOwnProperty('showOnSelect')) {
-            el.container_assurance.style.visibility = 'visible';
-        } else {
-            el.container_assurance.style.visibility = 'hidden';
-        }
-  */      
     };
+
+    // main //////////////////////////////////////////////////////////////////
 
     el.selectedProductList.addEventListener('click', handleProductAction);
     el.achat_licence.addEventListener('change', handlerAssuranceChoice);
+    handlerAssuranceChoice(); // force render update
+
+    // add function "addToSelectedProducts" to the global scop so it can be used by selectizejs 
+    // when user selects a product item 
     // @ts-ignore
     window.gymv = {
         addToSelectedProducts : addToSelectedProducts
     };
-    
 })();
