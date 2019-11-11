f265cf96d06ada8dc3380a9247ecd5e75b43eb59
raoul2000 <manu34@free.fr>
2019-11-11 15:31:19 +0100

    dashboard data

--- src/modules/gymv/controllers/DashboardController.php
+++ src/modules/gymv/controllers/DashboardController.php
@@ -2,11 +2,98 @@
 
 namespace app\modules\gymv\controllers;
 
+
+use Yii;
+use yii\web\NotFoundHttpException;
+use \app\models\Contact;
+use \app\models\ContactSearch;
+use \app\models\Product;
+use \app\models\Order;
+use \app\models\BankAccount;
+use \app\components\SessionDateRange;
+use \app\components\SessionContact;
+use app\modules\gymv\models\ProductSelectionForm;
+
 class DashboardController extends \yii\web\Controller
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => \yii\filters\VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+            'access' => [
+                'class' =>  \yii\filters\AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ]
+                ],
+            ],
+        ];
+    }
+
     public function actionIndex()
     {
-        return $this->render('index');
+        // build cards info
+        // find all contact Id for contact who have consumed date range valid orders for products 51 or 52
+        /*
+        $membersContactIds = Order::find()
+            ->select('to_contact_id')
+            ->validInDateRange(SessionDateRange::getStart(), SessionDateRange::getEnd())
+            ->where(['in', 'product_id', [51,52]])
+            ->distinct()
+            ->all();
+        */
+
+        $members = Contact::find()
+            ->select('id')
+            ->where(['is_natural_person' => true])
+            ->with([
+                'toOrders' => function ($query) {
+                    $query
+                        ->andWhere(['in', 'product_id', [52,53]])
+                        ->validInDateRange(
+                            SessionDateRange::getStart(), 
+                            SessionDateRange::getEnd()
+                        );
+                },
+            ])
+            ->all();
+        $memberIds = array_map(function($contact) {
+            return $contact->id;
+        }, $members);
+
+        $membersCount = count($memberIds);
+
+        // bank account balance info
+        $bankAccount = BankAccount::findOne(SessionContact::getBankAccountId());
+        $balanceInfo = $bankAccount->getBalanceInfo();
+        
+
+        // Order consmued belonging to 
+        $courseProductIds = ProductSelectionForm::getProductIdsByGroup(ProductSelectionForm::GROUP_COURSE);
+        $countCourses = Order::find()
+            ->validInDateRange(SessionDateRange::getStart(), SessionDateRange::getEnd())
+            ->where(['in', 'product_id', $courseProductIds] )
+            ->andWhere(['in', 'to_contact_id', $memberIds])
+            ->distinct()
+            ->count();
+
+        return $this->render('index', [
+            'membersCount' => $membersCount,
+            'solde' => $balanceInfo['value'],
+            'totalDeb' => $balanceInfo['totalDeb'],
+            'totalCred' => $balanceInfo['totalCred'],
+            'countCourses' => $countCourses
+        ]);
     }
 
 }
--- src/modules/gymv/views/dashboard/index.php
+++ src/modules/gymv/views/dashboard/index.php
@@ -7,21 +7,21 @@ $this->registerCss(file_get_contents(__DIR__ . '/dashboard.css'));
         <ul class="cardboard">
             <li class="card blue">
                 <h1>Adhérents</h1>
-                <div class="metric">433</div>
+                <div class="metric"><?= $membersCount ?></div>
                 <div class="detail">
-                    percent update 1 hour ago
+                    Vincennois et non Vincennois sur la saison
                 </div>
             </li>
             <li class="card blue">
                 <h1>Compte Courant</h1>
-                <div class="metric">2334 eurs</div>
+                <div class="metric"><?= $solde ?></div>
                 <div class="detail">
-                    Total débit = 2334, Total crédit = 3345,45 sdf sdf
+                    Total débit = <?= $totalDeb ?>, Total crédit = <?= $totalCred ?>
                 </div>
             </li>
             <li class="card blue">
                 <h1>Cours Achetés</h1>
-                <div class="metric">570</div>
+                <div class="metric"><?= $countCourses ?></div>
                 <div class="detail">
                     Nombre total de cours sur la saison
                 </div>
